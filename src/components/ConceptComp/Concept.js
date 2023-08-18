614ac93073ac8f50fe184888564fccea3d15a231
Kanha Tomar <kanharajput91@gmail.com>
2023-08-18 12:27:30 +0530

    cards created using composition

--- src/components/ConceptComp/Concept.js
+++ src/components/ConceptComp/Concept.js
@@ -1,12 +1,14 @@
 import EventImg from '../../assets/images/events.png';
+import Card from '../../UI/Card';
+import './Concept.css';
 
 function Concept(props){
     return(
-        <div>
+        <Card className="concept">
             <img src={EventImg} />
             <h2>{props.concept.title}</h2>
-            <h2>{props.concept.description}</h2>
-        </div>
+            <p>{props.concept.description}</p>
+        </Card>
     );
 }
 

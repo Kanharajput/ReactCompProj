66962d82ccea60a91beb21a3c31423c882fd1820
Kanha Tomar <kanharajput91@gmail.com>
2023-08-18 12:56:50 +0530

    pass the images in props

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
   const concepts = [
     {
       title: 'Components',
-      image: {ComponentImg},
+      image: ComponentImg,
       description:
         'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
     },
     {
       title: 'State',
-      image: {StateImg},
+      image: StateImg,
       description:
         'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
     },
     {
       title: 'Events',
-      image: {EventImg},
+      image: EventImg,
       description:
         'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
     },
--- src/components/conceptcomp/Concept.js
+++ src/components/conceptcomp/Concept.js
@@ -1,12 +1,11 @@
-import EventImg from '../../assets/images/events.png';
 import Card from '../ui/Card';
 import './Concept.css';
 
 function Concept(props){
     return(
         <Card className="concept">
-            <img src={EventImg} />
-            <h2>{props.concept.title}</h2>
+            <img src={props.concept.image} />
+            <h3>{props.concept.title}</h3>
             <p>{props.concept.description}</p>
         </Card>
     );
